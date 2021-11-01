b753307b79e84fedb8f08c06b65622b8dfc946a9
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:23:53 -0400

    Copy the add page view code from MacWikiDemo.

--- Shared/AddPageView.swift
+++ Shared/AddPageView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct AddPageView: View {
+    @Binding var wiki: Wiki
+    var newPage = Page(text: "")
+    @State private var newPageTitle = "New Page"
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Enter the name for the new page")
+                .font(.headline)
+            TextField("Name", text: $newPageTitle)
+                .padding(.vertical)
+            
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+
+                }, label: {
+                    Text("Cancel")
+                })
+                .keyboardShortcut(.cancelAction)
+
+                Spacer()
+                
+                Button( action: {
+                    presentationMode.wrappedValue.dismiss()
+                    wiki.addPage(title: newPageTitle)
+                }, label: {
+                    Text("Add Page")
+                })
+                .keyboardShortcut(.defaultAction)
+            }
+        }
+        .padding()
+        
     }
 }
 
 struct AddPageView_Previews: PreviewProvider {
     static var previews: some View {
-        AddPageView()
+        AddPageView(wiki: .constant(Wiki()))
     }
 }
