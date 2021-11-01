7ecbc42419e107144a5583905cd974e8e14c1e6d
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:20:32 -0400

    Copy the page list view code from MacWikiDemo.

--- macOS/PageListView.swift
+++ macOS/PageListView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct PageListView: View {
+    @Binding var wiki: Wiki
+    @State private var selectedPage: Page? = nil
+    @State private var showAddSheet = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Pages")
+                .font(.title)
+            List($wiki.pages) { $page in
+                NavigationLink(destination: PageView(page: $page),
+                               tag: page,
+                               selection: $selectedPage) {
+                    TextField("", text: $page.title)
+                }
+            }
+            // Select the first page when creating or opening a wiki.
+            .onAppear {
+                selectedPage = wiki.pages.first
+            }
+            .onDeleteCommand {
+                if selectedPage != nil {
+                    wiki.removePage(title: selectedPage!.title)
+                }
+                
+            }
+            HStack {
+                Button(action: { showAddSheet = true }, label: {
+                    Label("Add", systemImage: "note.text.badge.plus")
+                })
+
+                Spacer()
+
+                Button(action: {
+                    if selectedPage != nil {
+                        wiki.removePage(title: selectedPage!.title)
+                    }
+                    
+                }, label: {
+                    Label("Delete", systemImage: "trash")
+                        .foregroundColor(.red)
+                })
+                    .keyboardShortcut(.delete, modifiers: [])
+            }
+            .padding(.horizontal, 6)
+            .padding(6)
+        }
+        
+        .sheet(isPresented: $showAddSheet) {
+            AddPageView(wiki: $wiki)
+        }
     }
+    
 }
 
 struct PageListView_Previews: PreviewProvider {
     static var previews: some View {
-        PageListView()
+        PageListView(wiki: .constant(Wiki()))
     }
 }
