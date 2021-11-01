9498e8d630abc428362e1032c480c53b95fd8343
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:15:21 -0400

    Add the page code from MacWikiDemo.

--- Shared/Page.swift
+++ Shared/Page.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class Page: Identifiable, Equatable, Hashable {
+    let id = UUID()
+    var title: String = "Page"
+    var text: String
+
+    init(text: String = "Type here") {
+        self.text = text
+    }
+    
+    static func == (lhs: Page, rhs: Page) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(title)
+        hasher.combine(text)
+    }
+    
+    func write() -> Data? {
+        return text.data(using: .utf8)
+    }
+    
+    func read(data: Data) {
+        if let fileContents = String(data: data, encoding: .utf8) {
+            text = fileContents
+        }
+    }
+}
