3ba3be833378f0579cfdb8b56ccb4dda8e455c2e
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:29:55 -0400

    Add web view code for the iOS app target.

--- iOS/WebView.swift
+++ iOS/WebView.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var html: String
+    
+    init(html: String) {
+        self.html = html
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.loadHTMLString(html, baseURL: nil)
+    }
+}
