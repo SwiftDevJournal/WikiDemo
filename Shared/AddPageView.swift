40307833503aaf659ee1d63d2dae4774ef323321
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:23:10 -0400

    Add a file for the add page view.

---
+++ Shared/AddPageView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPageView.swift
+//  WikiDemo
+//
+//  Created by mark on 11/1/21.
+//
+
+import SwiftUI
+
+struct AddPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPageView()
+    }
+}
