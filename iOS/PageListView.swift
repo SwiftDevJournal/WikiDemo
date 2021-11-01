59c7ad64fd1df226695eba85b61cbcde3d9e63ec
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:30:54 -0400

    Add a page list view file to the iOS app target.

---
+++ iOS/PageListView.swift
@@ -0,0 +1,20 @@
+//
+//  PageListView.swift
+//  WikiDemo
+//
+//  Created by mark on 11/1/21.
+//
+
+import SwiftUI
+
+struct PageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageListView()
+    }
+}
