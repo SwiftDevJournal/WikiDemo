d0d3178a4c040573205ab1089e08157fc924d2c6
Mark Szymczyk <mark@swiftdevjournal.com>
2021-11-01 14:19:58 -0400

    Add a page view file to the Mac app target.

---
+++ macOS/PageView.swift
@@ -0,0 +1,20 @@
+//
+//  PageView.swift
+//  WikiDemo
+//
+//  Created by mark on 11/1/21.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
