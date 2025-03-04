d8f1a9aaf0aa4da93740d679b86e8119f45a3068
Swift Dev Journal <mark@swiftdevjournal.com>
2025-03-03 21:51:40 -0500

    Update the navigation link on the iOS version to use the new APIs.

--- iOS/PageListView.swift
+++ iOS/PageListView.swift
@@ -9,35 +9,35 @@ import SwiftUI
 
 struct PageListView: View {
     @Binding var wiki: Wiki
-    @State private var selectedPage: Page? = nil
+    @Binding var selectedPage: Page?
     @State private var showAddSheet = false
     @Environment(\.editMode) private var editMode
     @State private var disableTextField = true
     
     var body: some View {
         VStack {
-            Text("Pages")
-                .font(.title)
-            List {
-                ForEach($wiki.pages) { $page in
-                    NavigationLink(destination: PageView(page: $page),
-                                   tag: page,
-                                   selection: $selectedPage) {
-                        // Only allow changing the page's title when someone taps the Edit button.
-                        // I found that too many times tapping on a page title takes me to changing the title when what I really want is to edit the page. People are going to be editing pages way more than renaming them. It makes sense to only allow page renaming when tapping the Edit button.
-                        TextField("", text: $page.title)
-                            .disabled(disableTextField)
-                            .onChange(of: editMode?.wrappedValue) { newValue in
-                                if (newValue != nil) && (newValue!.isEditing) {
-                                    disableTextField = false
+            List(selection: $selectedPage) {
+                Section(header: Text("Pages")) {
+                    ForEach($wiki.pages, id: \.id) { $page in
+                        NavigationLink(value: page) {
+                            // Only allow changing the page's title when someone taps the Edit button.
+                            // I found that too many times tapping on a page title takes me to changing the title when what I really want is to edit the page. People are going to be editing pages way more than renaming them. It makes sense to only allow page renaming when tapping the Edit button.
+                            TextField("", text: $page.title)
+                                .disabled(disableTextField)
+                                .onChange(of: editMode?.wrappedValue) { oldValue, newValue in
+                                    if (newValue != nil) && (newValue!.isEditing) {
+                                        disableTextField = false
+                                    }
+                                    else {
+                                        disableTextField = true
+                                    }
                                 }
-                                else {
-                                    disableTextField = true
-                                }
-                            }
+                        }
                     }
+                    .onDelete(perform: deletePage)
                 }
-                .onDelete(perform: deletePage)
+                
+                
             }
             .listStyle(SidebarListStyle())
             .navigationTitle("Pages")
@@ -59,10 +59,8 @@ struct PageListView: View {
     }
 }
     
-
-
 struct PageListView_Previews: PreviewProvider {
     static var previews: some View {
-        PageListView(wiki: .constant(Wiki()))
+        PageListView(wiki: .constant(Wiki()), selectedPage: .constant(Page(text: "Page List")))
     }
 }
